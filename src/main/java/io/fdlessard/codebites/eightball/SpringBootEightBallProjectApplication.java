6d7ccae1f4a1ec4914e7ee119d1c46cdc42ffcd8
fdlessard <fdlessard@gmail.com>
2016-11-25 21:33:13 -0500

    first commit

---
+++ src/main/java/io/fdlessard/codebites/eightball/SpringBootEightBallProjectApplication.java
@@ -0,0 +1,12 @@
+package io.fdlessard.codebites.eightball;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootEightBallProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootEightBallProjectApplication.class, args);
+	}
+}
