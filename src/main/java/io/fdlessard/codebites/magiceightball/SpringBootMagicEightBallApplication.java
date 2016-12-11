7765f1b47587c052a75b0d1ea8ce053c55c1901c
fdlessard <fdlessard@gmail.com>
2016-12-11 14:44:12 -0500

    Fixed configs

--- src/main/java/io/fdlessard/codebites/magiceightball/SpringBootMagicEightBallApplication.java
+++ src/main/java/io/fdlessard/codebites/magiceightball/SpringBootMagicEightBallApplication.java
@@ -2,11 +2,13 @@ package io.fdlessard.codebites.magiceightball;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 
 @SpringBootApplication
-//@EnableEurekaClient
+//@EnableDiscoveryClient
+@EnableEurekaClient
 public class SpringBootMagicEightBallApplication {
 
 	public static void main(String[] args) {
